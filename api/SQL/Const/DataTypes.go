d49d328debb0051bb5a45b8ec290529142eecd3b
danushka96 <danushkaherath96@gmail.com>
2020-02-26 19:38:53 +0530

    inital fileset

---
+++ api/SQL/Const/DataTypes.go
@@ -0,0 +1,8 @@
+package Const
+
+const (
+	Varchar = "VARCHAR"
+	Integer = "INTEGER"
+	Float = "FLOAT(8,2)"
+	Double = "DOUBLE(8,2)"
+)
---
+++ api/SQL/insert.go
@@ -0,0 +1,3 @@
+package SQL
+
+const insertQuerySql = "insert into ? "
